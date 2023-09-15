4827dbc7a95a687b949ed565a1f7350833a58ced
RevengeMeds <145170212+RevengeMeds@users.noreply.github.com>
2023-09-16 00:58:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+$logfile = "visits.log";
 
+$ip = $_SERVER['REMOTE_ADDR'];
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'No referrer';
+$timestamp = date("Y-m-d H:i:s");
+$log_entry = "IP: $ip | Browser: $userAgent | Referer: $referer | Time: $timestamp" . PHP_EOL;
+
+file_put_contents($logfile, $log_entry, FILE_APPEND);
+
+echo "IP address, browser, and referer logged.";
+?>
